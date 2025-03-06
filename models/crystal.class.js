ddaa3abffb8ae9e0ee589fa896e4e7b4fea8fabd
Stephanie Englberger <stephaniekoebich@aol.com>
2025-03-06 16:13:52 +0100

    Adapted crystals position

--- models/crystal.class.js
+++ models/crystal.class.js
@@ -1,20 +1,32 @@
 class Crystal extends CollectableObject {
+  static lastX = 700;
+
   constructor(x, y) {
     super().setImage("assets/img/game_objects/fire-crystal.png");
-    this.x = x || 1000 + Math.random() * 2500;
+    this.x = x || this.generateXPosition();
     this.y = y || 150 + Math.random() * 150;
-
   }
 
-    /**
+  /**
+   * Generates an x position for the crystal based on the previous crystal's position.
+   * @method generateXPosition
+   * @returns {number} The generated x position.
+   */
+  generateXPosition() {
+    let newX = Crystal.lastX + 300 + Math.random() * 200;
+    Crystal.lastX = newX;
+    return newX;
+  }
+
+  /**
    * Creates a crystal with fixed x-position
    * @param {number} x - The fixed x-position
    * @returns {Crystal} - The crystal with the fixed x-position
    */
-    static createFixedCrystal(x, y) {
-      const crystal = new Crystal(x, y);
-      crystal.x = x;
-      crystal.y = y;
-      return crystal;
-    }
+  static createFixedCrystal(x, y) {
+    const crystal = new Crystal(x, y);
+    crystal.x = x;
+    crystal.y = y;
+    return crystal;
+  }
 }
