d6de585ec1d3093f488458b9d1a349884bfaeb6b
Stephanie Englberger <stephaniekoebich@aol.com>
2025-03-07 14:37:44 +0100

    "Cleaned sounds function"

--- js/sounds.js
+++ js/sounds.js
@@ -1,18 +1,15 @@
-const sounds = {
+let sounds = {
   character: {
     attack: new Audio("assets/audio/fireball-sound.mp3"),
     hurt: new Audio("assets/audio/elara_hurting_sound.mp3"),
     jump: new Audio("assets/audio/elara_jumping_sound.mp3"),
     ko: new Audio("assets/audio/elara_defeated_sound.mp3"),
+    footsteps: new Audio("assets/audio/elara_footsteps.mp3"),
   },
   blueSlime: {
-    attack: new Audio(),
-    hurt: new Audio(),
     ko: new Audio("assets/audio/slime_defeated.mp3"),
   },
   littleDragon: {
-    attack: new Audio(),
-    hurt: new Audio(),
     ko: new Audio("assets/audio/littleDragon_defeated.mp3"),
   },
   dragonBoss: {
@@ -20,7 +17,7 @@ const sounds = {
     attack: new Audio(),
     hurt: new Audio(),
   },
-  environment: { background: new Audio("assets/audio/bg-music-lvl1.mp3"), owlHootings: new Audio(), footsteps: new Audio() },
+  environment: { background: new Audio("assets/audio/bg-music-lvl1.mp3"), owlHootings: new Audio()},
   collectibles: {
     crystal: new Audio("assets/audio/collectCrystal.mp3"),
     flower: new Audio("assets/audio/collectFlower.mp3"),
