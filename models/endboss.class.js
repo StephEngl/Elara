d3280da3107ef5bc79aa00fb9bc931721a776fca
Stephanie Englberger <stephaniekoebich@aol.com>
2025-03-08 21:21:44 +0100

    Extended Endboss class with state handler

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -38,14 +38,6 @@ class Endboss extends MovableObject {
     "assets/img/enemies/endboss/dragon/Attack3.png",
     "assets/img/enemies/endboss/dragon/Attack4.png",
   ];
-  imagesFireattack = [
-    "assets/img/enemies/endboss/dragon/Fire_Attack1.png",
-    "assets/img/enemies/endboss/dragon/Fire_Attack2.png",
-    "assets/img/enemies/endboss/dragon/Fire_Attack3.png",
-    "assets/img/enemies/endboss/dragon/Fire_Attack4.png",
-    "assets/img/enemies/endboss/dragon/Fire_Attack5.png",
-    "assets/img/enemies/endboss/dragon/Fire_Attack6.png",
-  ];
   imagesHurt = [
     "assets/img/enemies/endboss/dragon/Hurt1.png",
     "assets/img/enemies/endboss/dragon/Hurt2.png",
@@ -58,6 +50,16 @@ class Endboss extends MovableObject {
     "assets/img/enemies/endboss/dragon/Death5.png",
   ];
   hadFirstContact = false;
+  EnemyState = {
+    INTRO: "intro",
+    IDLE: "idle",
+    WALKING: "walking",
+    ATTACKING: "attacking",
+    HURT: "hurting",
+    DYING: "dying",
+  };
+  fireballCooldown = 2000; // 2 seconds between attacks
+  lastFireballTime = 0;
 
   constructor() {
     super();
@@ -65,7 +67,9 @@ class Endboss extends MovableObject {
     this.loadAllImages();
     this.loadAudio();
     this.setObjectProperties();
-    this.animate();
+    this.state = this.EnemyState.INTRO;
+    this.stateTimer = 0;
+    this.isActive = false;
   }
 
   /**
@@ -77,7 +81,6 @@ class Endboss extends MovableObject {
     this.loadImages(this.imagesIdle);
     this.loadImages(this.imagesWalking);
     this.loadImages(this.imagesAttack);
-    this.loadImages(this.imagesFireattack);
     this.loadImages(this.imagesDying);
   }
 
@@ -88,8 +91,8 @@ class Endboss extends MovableObject {
   setObjectProperties() {
     this.height = 600;
     this.width = 600;
-    this.speed = 10;
-    this.x = 4000; //3800
+    this.speed = 20;
+    this.x = 1000; //3800
     this.y = -40;
     this.offset = {
       top: 300,
@@ -106,32 +109,138 @@ class Endboss extends MovableObject {
    */
   loadAudio() {
     this.audioEndbossRoar = sounds.dragonBoss.roar;
+    this.audioEndbossHurt = sounds.dragonBoss.hurt;
+    this.audioEndbossFire = sounds.dragonBoss.attack;
+    this.audioEndbossDefeated = sounds.dragonBoss.ko;
   }
 
-  /**
-   * Animates the Endboss, including intro and idle animations.
-   * @method animate
-   */
-  animate() {
-    let i = 0;
-    setInterval(() => {
-      if (i > 10) {
-        this.playAnimation(this.imagesIdle);
-      } else {
-        this.playAnimation(this.imagesIntro);
-        this.moveLeft(this.speed);
-      }
-      i++;
-      if (world.character.x > 3300 && !this.hadFirstContact) {
-        i = 0;
-        this.hadFirstContact = true;
-        sounds.dragonBoss.roar.play();
-        sounds.dragonBoss.roar.playbackRate = 0.8;
-      }
-      if (this.isDying) {
-        this.playAnimation(this.imagesDying);
-      }
-    }, 250);
+  activate() {
+    this.isActive = true;
+    this.initializeStateMachine();
+  }
+
+  initializeStateMachine() {
+    setInterval(() => this.updateStateMachine(), 250);
+  }
+
+  updateStateMachine() {
+    if (!this.isActive) return;
+    if (this.isDying) {
+      this.handleDyingState();
+      return;
+    }
+
+    if (this.isHurted) {
+      this.handleHurtState();
+      return;
+    }
+
+    if (this.isPlayerInRange()) {
+      this.changeState(this.EnemyState.ATTACKING);
+    } else {
+      this.changeState(this.EnemyState.WALKING);
+    }
+
+    switch (this.state) {
+      case this.EnemyState.INTRO:
+        this.handleIntroState();
+        break;
+
+      case this.EnemyState.IDLE:
+        this.handleIdleState();
+        break;
+
+      case this.EnemyState.WALKING:
+        this.handleWalkingState();
+        break;
+
+      case this.EnemyState.ATTACKING:
+        this.handleAttackState();
+        break;
+
+      default:
+        console.error("Ungültiger Zustand:", this.state);
+    }
+  }
+
+  handleIntroState() {
+    this.audioEndbossRoar.play();
+    this.playAnimation(this.imagesIntro);
+    this.moveLeft(this.speed);
+
+    if (this.stateTimer++ > 8) {
+      this.changeState(this.EnemyState.IDLE);
+    }
+  }
+
+  handleIdleState() {
+    this.playAnimation(this.imagesIdle);
+
+    // Zustandsübergang nach 1.5 Sekunden
+    if (this.stateTimer++ > 6) {
+      // 6 * 250ms = 1500ms
+      this.changeState(this.EnemyState.WALKING);
+    }
+  }
+
+  handleWalkingState() {
+    this.playAnimation(this.imagesWalking);
+    this.moveLeft(this.speed);
+
+    if (this.isPlayerInRange()) {
+      this.changeState(this.EnemyState.ATTACKING);
+    }
+  }
+
+  handleAttackState() {
+    this.playAnimation(this.imagesAttack);
+    this.checkFirebreathAttack();
+
+    // Angriffslogik hier
+    if (this.stateTimer++ > 8) {
+      // 4 * 250ms = 1000ms
+      this.changeState(this.EnemyState.WALKING);
+    }
+  }
+
+  changeState(newState) {
+    this.state = newState;
+    this.stateTimer = 0;
+  }
+
+  checkFirebreathAttack() {
+    if (this.isPlayerInRange() && this.canFire()) {
+      this.createDragonFire();
+      this.lastFireballTime = Date.now();
+    }
+  }
+
+  canFire() {
+    return Date.now() - this.lastFireballTime > this.fireballCooldown;
+  }
+
+  createDragonFire() {
+    const fireball = new FlyingObject(
+      this.x + 100,
+      this.y + 180,
+      true,
+      true
+    );
+    world.flyingObjects.push(fireball);
+    this.audioEndbossFire.play();
+  }
+
+  changeState(newState) {
+    this.state = newState;
+    this.stateTimer = 0;
+  }
+
+  isPlayerInRange() {
+    return Math.abs(this.x - world.character.x) < 150;
+  }
+
+  handleDyingState() {
+    this.playAnimation(this.imagesDying);
   }
 
   /**
@@ -140,12 +249,12 @@ class Endboss extends MovableObject {
    */
   die() {
     this.isDying = true;
-    audioEndbossRoar.play();
-    audioEndbossRoar.playbackRate = 0.8;
+    this.audioEndbossDefeated.play();
+    this.audioEndbossDefeated.playbackRate = 1.1;
     setTimeout(() => {
       this.shouldRemove = true;
       gameWon = true;
       stopGame();
-    }, 1000);
+    }, 900);
   }
 }
